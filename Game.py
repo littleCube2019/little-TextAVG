0102e68ed1c96614ace357dc9ee5bc248eb515bf
brad1024 <60173176+brad1024@users.noreply.github.com>
2020-01-24 10:16:38 +0800

    Add files via upload

--- Game.py
+++ Game.py
@@ -1,12 +1,15 @@
+import random
+
 def clearScreen():  # 換場景時使用
     for i in range(100):
         print("\n")
         
-class character():  # 紀錄角色狀態
+class character():  # 紀錄角色狀態(新增攻擊力)
 
      def __init__(self,name):
         self.name = name
         self.hp = 10
+        self.att = 1
 
 
 
@@ -16,8 +19,15 @@ class environment():  # 紀錄環境狀態
         self.day = 1
     
     def oneDayPass(self):
-        self.day +=1
+        self.day += 1
 
+
+
+class enemy(): # 紀錄敵人參數(新增)
+
+    def __init__(self,hp,att):
+        self.hp=hp
+        self.att=att
 
 def showState(inCharacter,inEnv): # 顯示狀態
     print("Today is day {}".format(inEnv.day))
@@ -30,12 +40,32 @@ def chooseAction(): # 選擇行動
         print("\n")
         print("1 Go to sleep")
         print("2 Do nothing")
-        choice=input("what do you want to do now?")
+        choice=input("what do you want to do now?\n")
         if choice == "1" or choice == "2":
             return choice
         else:
             clearScreen()
             print("invalid,please choice again!")
+
+
+def fight(): # 戰鬥系統(新增)
+    choice = 0
+    damage = 0
+    while 1:
+        print("\n")
+        print("1 Attact")
+        print("2 Run")
+        choice=input("What do you want to do now?\n")
+        if choice == "1":
+            damage = random.randint(1,2)
+            return damage
+        elif choice == "2":
+            damage = 0
+            return damage
+        else :
+            clearScreen()
+            print("invalid,please choice again!")
+        
 
 
 
@@ -45,6 +75,10 @@ def chooseAction(): # 選擇行動
 gameOver = False
 
 player = character("brad")
+
+littlezombie = enemy(5,1)  # 小僵僵數值(新增)
+
+bloodmoon = random.randint(2,6)  # 血月(新增)
 
 env = environment()
 
@@ -62,9 +96,31 @@ while not gameOver:
         print("You do nothing~")
     print()
 
-    
-    if env.day >= 8:
-        print("8 days to die!")
+    if env.day == bloodmoon: # 跟little zombie戰鬥(新增)
+        showState(player,env)
+        print("\nToday is BLOODMOON!!!\n")
+        while player.hp > 0 and littlezombie.hp > 0:
+            print("There is a little zombie in your house")
+            print("little zombie HP:{}".format(littlezombie.hp))
+            print("your HP:{}".format(player.hp))
+            damage = fight()
+            clearScreen()
+            print("yo do {} damage\n".format(damage))
+            littlezombie.hp -= damage
+            if littlezombie.hp > 0:
+                print("little zombie attact you")
+                print("you lose 1 HP\n")
+                player.hp -= 1
+        if player.hp <= 0:
+            print("you die\n")
+            gameOver=True
+        elif littlezombie.hp <= 0:
+            print("you kill the little zombie\n")
+            
+
+
+    if env.day >= 7:
+        print("7 days to die!")
         print("game is over")
         gameOver=True
 
