18ce4e713d25411aa451db4c4833985e17095b30
brad1024 <60173176+brad1024@users.noreply.github.com>
2020-02-03 13:57:28 +0800

    換行規範是啥

--- Game.py
+++ Game.py
@@ -6,10 +6,11 @@ def clearScreen():  # 換場景時使用
         
 class character():  # 紀錄角色狀態(新增攻擊力)
 
-     def __init__(self,name,hp,att):
+     def __init__(self,name,hp,atk,deff):
         self.name = name
         self.hp = hp
-        self.att = att
+        self.atk = atk
+        self.deff = deff
 
 
 
@@ -23,12 +24,6 @@ class environment():  # 紀錄環境狀態
 
 
 
-class enemy(): # 紀錄敵人參數(新增)
-
-    def __init__(self,hp,att):
-        self.hp=hp
-        self.att=att
-
 def showState(inCharacter,inEnv): # 顯示狀態
     print("Today is day {}".format(inEnv.day))
     print("Player: {} \nhp: {}".format(inCharacter.name, inCharacter.hp))
@@ -36,60 +31,68 @@ def showState(inCharacter,inEnv): # 顯示狀態
 
 def chooseAction(): # 選擇行動
     choice = 0
-    damage = 0
-    if bloodMoon==False:
-        while 1:
-            print("\n")
+    if not bloodMoon:
+        choice = normalDay()
+    elif bloodMoon:
+        choice = bloodDay()
+    return choice
+    
+
+def normalDay():
+    choice = 0
+    while 1:
+            print("what do you want to do now?\n")
             print("1 Go to sleep")
             print("2 Do nothing")
-            choice=input("what do you want to do now?\n")
+            choice = input()
             if choice == "1" or choice == "2":
                 return choice
             else:
                 clearScreen()
                 print("invalid,please choice again!")
-    elif bloodMoon==True:
-        while 1:
-            print("\n")
+
+def bloodDay():
+    choice = 0
+    while 1:
+            print("What do you want to do now?\n")
             print("1 Attact")
             print("2 Run")
-            choice=input("What do you want to do now?\n")
-            if choice == "1":
-                damage = random.randint(1,2)
-                return damage
-            elif choice == "2":
-                damage = 0
-                return damage
-            else :
+            choice = input()
+            if choice == "1" or choice == "2":
+                return choice
+            else:
                 clearScreen()
                 print("invalid,please choice again!")
 
 
+def doDamage(aAtk,bDeff):
+    damage = aAtk*random.randint(1,2)-bDeff
+    if damage <= 0:
+        return 0
+    else:
+        return damage
 
 
 
-
-
-day = 7
+DAY = 7
 
 gameOver = False
 
-player = character("brad",10,1)
+player = character("brad",100,10,1)
 
-littleZombie = character("littleZombie",5,1)  # 小僵僵數值(新增)
+littleZombie = character("littleZombie",50,5,0)  # 小僵僵數值(新增)
 
-bloodMoonDay = random.randint(2,day)  # 血月(新增)
+bloodMoonDay = random.randint(2,DAY)  # 血月(新增)
 
 env = environment()
 
 bloodMoon = False
 
+
 while not gameOver:
     
     
-
-    
-    if env.day==bloodMoonDay:
+    if env.day == bloodMoonDay:
         bloodMoon = True
     elif env.day!=bloodMoonDay:
         bloodMoon = False
@@ -111,14 +114,21 @@ while not gameOver:
             print("There is a little zombie in your house")
             print("little zombie HP:{}".format(littleZombie.hp))
             print("your HP:{}".format(player.hp))
-            damage = chooseAction()
-            clearScreen()
-            print("yo do {} damage\n".format(damage))
+            ans = chooseAction()
+            if ans == "1":
+                clearScreen()
+                damage = doDamage(player.atk,littleZombie.deff)
+                print("yo do {} damage\n".format(damage))
+            elif ans == "2":
+                clearScreen()
+                damage = 0
+                print("YOU SCREAM AND RUN!!!\n")
             littleZombie.hp -= damage
             if littleZombie.hp > 0:
                 print("little zombie attact you")
-                print("you lose 1 HP\n")
-                player.hp -= 1
+                damage = doDamage(littleZombie.atk,player.deff)
+                print("you lose {} HP\n".format(damage))
+                player.hp -= damage
         if player.hp <= 0:
             print("you die\n")
             gameOver=True
@@ -132,7 +142,7 @@ while not gameOver:
     if env.day >= 7:
         print("7 days to die!")
         print("game is over")
-        gameOver=True
+        gameOver = True
 
 print("Game over")
 
